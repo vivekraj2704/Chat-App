63c85be2d7b8c077beabd8f2739628abb6ce4f1c
vivekraj2704 <rajvivek2704@gmail.com>
2024-08-26 19:29:59 +0530

    socket io implemented

--- frontend/src/components/sidebar/Conversation.jsx
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -1,12 +1,12 @@
-// import { useSocketContext } from "../../context/SocketContext";
+import { useSocketContext } from "../../context/SocketContext";
 import useConversation from "../../zustand/useConversation";
 
 const Conversation = ({ conversation, lastIdx, emoji }) => {
 	const { selectedConversation, setSelectedConversation } = useConversation();
 
 	const isSelected = selectedConversation?._id === conversation._id;
-	// const { onlineUsers } = useSocketContext();
-	// const isOnline = onlineUsers.includes(conversation._id);
+	const { onlineUsers } = useSocketContext();
+	const isOnline = onlineUsers.includes(conversation._id);
 
 	return (
 		<>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,12 +4,15 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { AuthContextProvider } from './context/AuthContext.jsx'
+import { Socket } from 'socket.io'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
     <AuthContextProvider>
+      <Socket>
       <App />
+      </Socket>
     </AuthContextProvider>
     </BrowserRouter>
   </StrictMode>,
